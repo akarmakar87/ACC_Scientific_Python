08c60277ea501131bf0966c5a2e6fe238a444eae
Asha Karmakar <asha.karmakar03@gmail.com>
2020-08-09 09:07:50 -0500

    first commit

---
+++ Assignment 05/operators.py
@@ -0,0 +1,49 @@
+# Objective: Demonstrate arithmetic operations
+
+print("REQUIREMENT 4 OUTPUT:\n")
+
+# Addition
+x = 1 + 2
+print (x,"= 1 + 2")
+
+# Subtraction
+x = 3 - 4
+print (x,"= 3 - 4")
+
+# Multiplication
+x = 5 * 6
+print (x,"= 5 * 6")
+
+# True division
+x = 7 / 8
+print (x,"= 7 / 8")
+
+# Floor division
+x = 8 // 7
+print (x,"= 8 // 7")
+
+# Exponentiation
+x = 2 ** 3
+print (x,"= 2 ** 3")
+
+# Objective: Demonstrate comparison operations
+
+print("\nREQUIREMENT 5 OUTPUT:\n")
+
+x = (1 == 2)
+print ("1 == 2 is",x)
+
+x = (3 != 3)
+print ("3 != 3 is",x)
+
+x = (4 < 5)
+print ("4 < 5 is",x)
+
+x = (7 > 8)
+print ("7 > 8 is",x)
+
+x = (9 <= 9)
+print ("9 <= 9 is",x)
+
+x = (10 >= 11)
+print ("10 >= 11 is",x)
---
+++ Assignment 08/myscript.py
@@ -0,0 +1,6 @@
+def cube(arg):
+    """Return the cube of the argument"""
+    return arg**3
+
+for i in range(5):
+    print (cube(i))
